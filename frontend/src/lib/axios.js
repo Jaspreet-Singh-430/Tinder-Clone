76a39438d8b08492d34f7e15e8c0218eecba7a53
Jaspreet-Singh-430 <jaspreet9322@gmail.com>
2026-05-08 12:01:43 +0530

    ready to be deployed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5000":"/"
+const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5000":"/backend"
 export const axiosInstance=axios.create(
     {
         baseURL:"http://localhost:5000/auth",
