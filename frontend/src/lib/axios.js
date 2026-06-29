32a0cdec1322d01392b7aa34fc1b890f06e8b01b
Jaspreet-Singh-430 <jaspreet9322@gmail.com>
2026-06-29 13:33:02 +0530

    changes have done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from "axios"
 const BASE_URL=import.meta.env.VITE_SERVER_URL
 export const axiosInstance=axios.create(
     {
-        baseURL:`${BASE_URL}/auth`,
+        baseURL:import.meta.env.VITE_MODE=="development" ? 
+        `${BASE_URL}/auth`:"/auth",
         withCredentials:true
     }
 )
